5d8f7664e3777723868d5204d75bb33582439591
martin <martin@Computadora>
2015-06-07 16:26:43 -0300

    gera puto

---
+++ newClass.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   newClass.cpp
+ * Author: martin
+ * 
+ * Created on 7 de junio de 2015, 04:26 PM
+ */
+
+#include "newClass.h"
+
+newClass::newClass() {
+}
+
+newClass::newClass(const newClass& orig) {
+}
+
+newClass::~newClass() {
+}
+
